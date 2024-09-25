441c717a901647d6731d860c554b2c35fc19f65a
surendragt19 <surendragt2002@gmail.com>
2024-09-25 23:29:33 +0530

    mail

--- config/sendMail.js
+++ config/sendMail.js
@@ -3,27 +3,41 @@ const nodemailer = require("nodemailer");
 const sendEmail = async ({ name, email, message }) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
-    secure:true,
-    port:465,
+    secure: true,
+    port: 465,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
 
-  const mailOptions = {
+  const mailOptionsToAdmin = {
     from: process.env.EMAIL_USER,
     to: "surendra203niet@gmail.com",
     subject: "New Message From Portfolio",
     html: `
-      <h1>Contact User Details: </h1>
+      <h1>Contact User Details:</h1>
       <p><strong>Name:</strong> ${name}</p>
       <p><strong>Email:</strong> ${email}</p>
       <p><strong>Message:</strong> ${message}</p>
     `,
   };
 
-  return transporter.sendMail(mailOptions);
+  const mailOptionsToUser = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Thank You for Connecting!",
+    html: `
+      <p>Dear ${name},</p>
+        <p>Thank you for reaching out! Your message has been received, and I will get back to you as soon as possible.</p>
+        <p>You can also connect with me on my LinkedIn profile <a href="https://www.linkedin.com/in/surendra-kumar-gupta-064294232/" target="_blank">Click Here!</a></p>
+        <p>Best Regards,</p>
+        <p>Surendra Kumar Gupta</p>
+    `,
+  };
+
+  await transporter.sendMail(mailOptionsToAdmin);
+  return transporter.sendMail(mailOptionsToUser);
 };
 
 module.exports = sendEmail;
